22950304bf0949c2bbf9d40bf83b050938e17c91
Tharanipathi <tharanipathi18@gmail.com>
2026-03-12 11:53:42 +0530

    Fix predict_loan function

--- src/predict.py
+++ src/predict.py
@@ -1,13 +1,22 @@
 import os
 import pickle
+import pandas as pd
 import gdown
 
 MODEL_PATH = "model/random_forest_model.pkl"
 
 if not os.path.exists(MODEL_PATH):
     os.makedirs("model", exist_ok=True)
-
-    url = "https://drive.google.com/file/d/1yUHf5QEQAUK-VsVrMFg_koiO0JFlTVWp/view?usp=drive_link"
+    url = "https://drive.google.com/uc?id=1yUHf5QEQAUK-VsVrMFg_koiO0JFlTVWp"
     gdown.download(url, MODEL_PATH, fuzzy=True)
 
 model = pickle.load(open(MODEL_PATH, "rb"))
+
+def predict_loan(data):
+
+    df = pd.DataFrame([data])
+
+    prediction = model.predict(df)[0]
+    probability = model.predict_proba(df)[0][1]
+
+    return prediction, probability
