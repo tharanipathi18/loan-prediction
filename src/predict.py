0a126c0ba2e06e5ecbf30c7d92266fa49f2b9575
Tharanipathi <tharanipathi18@gmail.com>
2026-03-12 11:05:55 +0530

    Add automatic model download from Google Drive

--- src/predict.py
+++ src/predict.py
@@ -1,33 +1,20 @@
+import os
 import pickle
 import pandas as pd
-import os
+import gdown
 
-# get project root
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+MODEL_PATH = "model/random_forest_model.pkl"
 
-# model paths
-model_path = os.path.join(BASE_DIR, "model", "random_forest_model.pkl")
-columns_path = os.path.join(BASE_DIR, "model", "model_columns.pkl")
+# download model if not present
+if not os.path.exists(MODEL_PATH):
+    os.makedirs("model", exist_ok=True)
 
-# load model and columns
-model = pickle.load(open(model_path, "rb"))
+    url = "https://drive.google.com/file/d/1yUHf5QEQAUK-VsVrMFg_koiO0JFlTVWp/view?usp=drive_link"
+    gdown.download(url, MODEL_PATH, quiet=False)
+
+# load model
+model = pickle.load(open(MODEL_PATH, "rb"))
+
+# load columns
+columns_path = "model/model_columns.pkl"
 model_columns = pickle.load(open(columns_path, "rb"))
-
-def predict_loan(data):
-
-    # convert input to dataframe
-    df = pd.DataFrame([data])
-
-    # encode categorical variables if present
-    df = pd.get_dummies(df)
-
-    # align with training columns
-    df = df.reindex(columns=model_columns, fill_value=0)
-
-    # prediction
-    prediction = model.predict(df)[0]
-
-    # probability
-    probability = model.predict_proba(df)[0][1]
-
-    return prediction, probability
